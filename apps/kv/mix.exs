b7d4f4a24bb47495c0f7967769b30af65d7f2488
Syver Enstad <syver.enstad@gmail.com>
2015-10-23 14:01:11 +0200

    Add socket server

--- apps/kv/mix.exs
+++ apps/kv/mix.exs
@@ -4,6 +4,8 @@ defmodule KV.Mixfile do
   def project do
     [app: :kv,
      version: "0.0.1",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
